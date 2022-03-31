12924d78de2a7694441c2bbcc46b8084865fbe27
Kasthury <t.kasthury66@gmail.com>
2022-04-01 02:00:34 +0530

    Home page completed

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -11,7 +11,9 @@ export default function Home() {
       <div className="mainSection">
         <span className="leftContent">
             <div className="general">General</div>
-            <div className="welcomeText">Health is your prioroty</div>
+            <div className="welcomeText"><p>“Values are related to our emotions, 
+                                          just as we practice physical hygiene to preserve our physical health, 
+                                          we need to observe emotional hygiene to preserve a healthy mind and attitudes.”</p></div>
             <button className="login">Login into your account</button>
         </span>
       </div>
