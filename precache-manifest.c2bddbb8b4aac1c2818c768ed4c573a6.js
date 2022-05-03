42a2c77129c54a15ebe2abe4eb952868d430ea0a
Kasthury <t.kasthury66@gmail.com>
2022-05-03 14:23:59 +0530

    Updates

---
+++ precache-manifest.c2bddbb8b4aac1c2818c768ed4c573a6.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "2168efe741a3d1ab288a",
+    "url": "/Portfolio/static/css/main.2510fe77.chunk.css"
+  },
+  {
+    "revision": "2168efe741a3d1ab288a",
+    "url": "/Portfolio/static/js/main.2168efe7.chunk.js"
+  },
+  {
+    "revision": "9d6d0ad7f97bf131636f",
+    "url": "/Portfolio/static/js/1.9d6d0ad7.chunk.js"
+  },
+  {
+    "revision": "a3aca903e4367b0b6d79",
+    "url": "/Portfolio/static/css/2.774bce1d.chunk.css"
+  },
+  {
+    "revision": "a3aca903e4367b0b6d79",
+    "url": "/Portfolio/static/js/2.a3aca903.chunk.js"
+  },
+  {
+    "revision": "1898b9cbe8750b1bc1f2",
+    "url": "/Portfolio/static/js/runtime~main.1898b9cb.js"
+  },
+  {
+    "revision": "67dba4225f2fa9eecae361c4dde5993e",
+    "url": "/Portfolio/static/media/pexels-cottonbro-4065885.67dba422.jpg"
+  },
+  {
+    "revision": "625b543c35c0856dd740fa39e543eb71",
+    "url": "/Portfolio/static/media/pexels-j-kelly-brito-4067119.625b543c.jpg"
+  },
+  {
+    "revision": "5cab54e60190a5c562ec5f3b79e1f4bf",
+    "url": "/Portfolio/static/media/pexels-jess-bailey-designs-788946.5cab54e6.jpg"
+  },
+  {
+    "revision": "4bd8a28e702295aeebbf46b09d403656",
+    "url": "/Portfolio/static/media/pexels-liza-summer-6347733.4bd8a28e.jpg"
+  },
+  {
+    "revision": "dc279bbd66e4da797c7bb4a386967e97",
+    "url": "/Portfolio/static/media/pexels-cottonbro-3945697.dc279bbd.jpg"
+  },
+  {
+    "revision": "e407c1d2f1aa5b7ad245c43bb465ae69",
+    "url": "/Portfolio/static/media/pexels-marek-levak-2265484.e407c1d2.jpg"
+  },
+  {
+    "revision": "71e68c7c9e63acbbf90ce257886ee893",
+    "url": "/Portfolio/static/media/pexels-sound-on-3756879.71e68c7c.jpg"
+  },
+  {
+    "revision": "c5dcfaedfac37519461d102462933530",
+    "url": "/Portfolio/static/media/webdesign.c5dcfaed.jpg"
+  },
+  {
+    "revision": "8c71eb8249f51af6cbeaf8070536df62",
+    "url": "/Portfolio/static/media/webdevelopment.8c71eb82.jpg"
+  },
+  {
+    "revision": "90623056761161d59ee2da451f2d3e42",
+    "url": "/Portfolio/static/media/graphicdesign.90623056.jpg"
+  },
+  {
+    "revision": "f6b0638d609fdb6cf9cfdf0c19dccd25",
+    "url": "/Portfolio/static/media/otherskills.f6b0638d.jpg"
+  },
+  {
+    "revision": "20eea501e482805ec0fea32ed7ac5847",
+    "url": "/Portfolio/index.html"
+  }
+];
